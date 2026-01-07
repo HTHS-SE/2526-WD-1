2893034e2b256ef31c174b81114a634c295590cf
michaeljykang <michaeljiayikang@gmail.com>
2026-01-07 13:00:26 -0500

    Added a template line chart
    1. Added a template linechart that shows the value using the firebase
    2. Added an addData.js file which I deleted last time, but is kept for now just in case

--- js/comparison.js
+++ js/comparison.js
@@ -28,3 +28,129 @@ const auth = getAuth()
 // Return instance of your app's firebase Realtime Databse (FRD)
 const db = getDatabase(app)
 
+// ---------------------------Get a country's data set --------------------------
+// Must be an async function because you need to get all the data from FRD
+// before you can process it for a table or graph
+async function getDataSet(country){
+
+  const countries = [];
+  const gdp = [];
+
+
+  const dbref = ref(db);  // firebase parameter to access database
+
+  await get(child(dbref, 'data/' + country)).then((snapshot) => {
+    if(snapshot.exists()){
+      console.log(snapshot.val());
+
+      snapshot.forEach(child => {
+        console.log(child.key, child.val());
+        // Push values to the corresponding arrays
+        countries.push(child.key);
+        gdp.push(child.val());
+      }); 
+    }
+    else {
+      alert('No data found');
+    }
+  })
+  .catch((error) => {
+    alert('Unsuccessful, error: ' + error);
+  });
+
+  return {countries, gdp};
+}
+
+
+async function createChart(){
+    const data = await getDataSet('United States'); // createChart will wait for getData() to process CSV
+    const lineChart = document.getElementById('lineChart1');
+
+    const myChart = new Chart(lineChart, {  // Construct the chart    
+        type: 'line',
+        data: {                         // Define data
+            labels: data.countries,       // x-axis labels
+            datasets: [                 // Each object describes one dataset of y-values
+                                        //  including display properties.  To add more datasets, 
+                                        //  place a comma after the closing curly brace of the last
+                                        //  data set object and add another dataset object. 
+                {
+                    label:    `GDP dollar value`,     // Dataset label for legend
+                    data:     data.gdp,    
+                    fill:     false,           // Fill area under the linechart (true = yes, false = no)
+                    backgroundColor:  'rgba(255, 0, 132, 0.2)',    // Color for data marker
+                    borderColor:      'rgba(255, 0, 132, 1)',      // Color for data marker border
+                    borderWidth:      1   // Data marker border width
+                }
+        ]
+        },
+        options: {                        // Define display chart display options 
+            responsive: true,             // Re-size based on screen size
+            maintainAspectRatio: true,
+            scales: {                     // Display options for x & y axes
+                x: {                      // x-axis properties
+                    title: {
+                        display: true,
+                        text: 'Year',     // x-axis title
+                        font: {                   // font properties
+                            size: 14
+                        },
+                    },
+                    ticks: {                      // x-axis tick mark properties
+                        callback: function(val, index){
+                            // Set the tick marks at every 5 years
+                            return index % 5 === 0 ? this.getLabelForValue(val) : '';
+                        },
+                        font: {
+                            size: 14  
+                        },
+                    },
+                    grid: {                       // x-axis grid properties
+                        color: '#6c767e'
+                    }
+                },
+                y: {                              // y-axis properties
+                    title: {
+                        display: true,                          
+                        text: `GDP in US dollar value`,     // y-axis title
+                        font: {
+                            size: 14
+                        },
+                    },
+                    ticks: {
+                        min: 0,                   
+                        maxTicksLimit: data.gdp.length,        // Actual value can be set dynamically
+                        font: {
+                            size: 12
+                        }
+                    },
+                    grid: {                       // y-axis gridlines
+                        color: '#6c767e'
+                    }
+                }
+            },
+            plugins: {                  // Display options for title and legend
+                title: {
+                    display: true,
+                    text: 'GDP vs. Year',
+                    font: {
+                        size: 24,
+                    },
+                    color: '#black',
+                    padding: {
+                        top: 10,
+                        bottom: 30
+                    }
+                },
+                legend: {
+                    align: 'start',
+                    position: 'bottom',
+                }
+            }
+        }       
+    });
+}
+
+window.onload = function(){
+  createChart();
+}
