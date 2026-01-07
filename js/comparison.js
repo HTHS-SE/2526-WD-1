58d532fa1ee5c277164520a312eec787a83b1e7e
michaeljykang <michaeljiayikang@gmail.com>
2026-01-07 12:10:02 -0500

    Added firebase config to comparison.js
    1. Added firebase config

---
+++ js/comparison.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getAuth } 
+  from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+
+import {getDatabase, ref, set, update, child, get, remove }
+  from "https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js"
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxTmX9T2G7XyjejGV6-8fQwE4bjGRL1pU",
+  authDomain: "us-gdp-project-se.firebaseapp.com",
+  databaseURL: "https://us-gdp-project-se-default-rtdb.firebaseio.com",
+  projectId: "us-gdp-project-se",
+  storageBucket: "us-gdp-project-se.firebasestorage.app",
+  messagingSenderId: "482979417992",
+  appId: "1:482979417992:web:821d52fd18d01f5931b43d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication
+const auth = getAuth()
+
+// Return instance of your app's firebase Realtime Databse (FRD)
+const db = getDatabase(app)
+
