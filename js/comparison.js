f2244ba0faf391fb6c4ac1bc7e5ddf7854c49292
michaeljykang <michaeljiayikang@gmail.com>
2026-01-13 14:13:18 -0500

    Added a third chart
    1. Added a third chart where custom data can be shown
    2. Added a getCustomDataSet function and a createChartSingle function that creates a chart using the user datasets

--- js/comparison.js
+++ js/comparison.js
@@ -102,6 +102,61 @@ async function getDataSet(country){
   return {years, gdp};
 }
 
+// ---------------------------Get a custom country's data set --------------------------
+// Must be an async function because you need to get all the data from FRD
+// before you can process it for a table or graph
+async function getCustomDataSet(userID, country){
+
+  const years = [];
+  const gdp = [];
+
+
+  const dbref = ref(db);  // firebase parameter to access database
+
+  await get(child(dbref, 'users/' + userID + '/data/' + country)).then((snapshot) => {
+    if(snapshot.exists()){
+      console.log(snapshot.val());
+
+      snapshot.forEach(child => {
+        console.log(child.key, child.val());
+        // Push values to the corresponding arrays
+        years.push(child.key);
+        gdp.push(child.val());
+      }); 
+    }
+    else {
+      alert('No data found');
+    }
+  })
+  .catch((error) => {
+    alert('Unsuccessful, error: ' + error);
+  });
+
+  return {years, gdp};
+}
+
+async function getCustomCountries(userID) {
+  const customCountries = [];
+
+  const dbref = ref(db);
+
+  await get(child(dbref, 'users/' + userID + '/data')).then((snapshot) => {
+    if(snapshot.exists()){
+      snapshot.forEach(child => {
+        customCountries.push(child.key);
+      });
+    }
+    else {
+      alert('No data found');
+    }
+  })
+  .catch((error) => {
+    alert('Unsuccessful, error: ' + error);
+  });
+
+  return customCountries;
+}
+
 async function getCountries() {
   const countries = [];
 
@@ -126,8 +181,101 @@ async function getCountries() {
   return countries;
 }
 
+async function createChartSingle(country, id, userID){
+  const data = country === "" ? {years: [], gdp: []} : await getCustomDataSet(userID, country); // createChart will wait for getCustomDataSet to retrieve the data
+  console.log(data);
+  const lineChart = document.getElementById(id);
+
+  return new Chart(lineChart, {  // Construct the chart    
+        type: 'line',
+        data: {                         // Define data
+            labels: data.years,       // x-axis labels
+            datasets: [                 // Each object describes one dataset of y-values
+                                        //  including display properties.  To add more datasets, 
+                                        //  place a comma after the closing curly brace of the last
+                                        //  data set object and add another dataset object. 
+                
+                {
+                    label:    `${country} GDP`,     // Dataset label for legend
+                    data:     data.gdp,    
+                    fill:     false,           // Fill area under the linechart (true = yes, false = no)
+                    backgroundColor:  'rgba(0, 132, 255, 0.2)',    // Color for data marker
+                    borderColor:      'rgba(255, 0, 132, 1)',      // Color for data marker border
+                    borderWidth:      1   // Data marker border width
+                }
+        ]
+        },
+        options: {                        // Define display chart display options 
+            responsive: true,             // Re-size based on screen size
+            maintainAspectRatio: true,
+            scales: {                     // Display options for x & y axes
+                x: {                      // x-axis properties
+                    type: 'linear',
+                    title: {
+                        display: true,
+                        text: 'Year',     // x-axis title
+                        font: {                   // font properties
+                            size: 14
+                        },
+                    },
+                    ticks: {                      // x-axis tick mark properties
+                        min: data.years[0],
+                        max: data.years[data.years.length-1],
+                        callback: function(val){
+                            return String(val);
+                        },  
+                        font: {
+                            size: 14  
+                        },
+                    },
+                    grid: {                       // x-axis grid properties
+                        color: '#6c767e'
+                    }
+                },
+                y: {                              // y-axis properties
+                    title: {
+                        display: true,                          
+                        text: `GDP`,     // y-axis title
+                        font: {
+                            size: 14
+                        },
+                    },
+                    ticks: {
+                        min: 0,                   
+                        maxTicksLimit: data.gdp.length,        // Actual value can be set dynamically
+                        font: {
+                            size: 12
+                        }
+                    },
+                    grid: {                       // y-axis gridlines
+                        color: '#6c767e'
+                    }
+                }
+            },
+            plugins: {                  // Display options for title and legend
+                title: {
+                    display: true,
+                    text: 'GDP vs. Year',
+                    font: {
+                        size: 24,
+                    },
+                    color: '#black',
+                    padding: {
+                        top: 10,
+                        bottom: 30
+                    }
+                },
+                legend: {
+                    align: 'start',
+                    position: 'bottom',
+                }
+            }
+        }       
+    });
+}
+
 async function createChart(country, id){
-    const dataUS = await getDataSet("United States"); // createChart will wait for getData() to process CSV
+    const dataUS = await getDataSet("United States"); // createChart will wait for getDataSet to retrieve the data
     const dataOther = country === "United States" ? {years: [], gdp: []} : await getDataSet(country);
     const lineChart = document.getElementById(id);
 
@@ -233,7 +381,10 @@ const countryParagraphs = {
 };
 const countrySelect = document.getElementById("country-select");
 const countryParagraph = document.getElementById("country-paragraph");
+let customCountries = [];
+const customSelect = document.getElementById("custom-select");
 let chart2;
+let chart3;
 
 window.addEventListener('load', async function(){
   countries = await getCountries();
@@ -248,16 +399,30 @@ window.addEventListener('load', async function(){
   console.log(countrySelect.value);
 
   getUsername();  // Get current user's first name
-  
+  const userID = currentUser.uid;
   document.getElementById('update').onclick = function(){
     const name = document.getElementById('customName').value;
     const year = document.getElementById('customYear').value;
     const gdp = document.getElementById('customGDP').value;
-    const userID = currentUser.uid;
+    const addDelete = document.getElementById('add-delete').value;
 
-    updateData(userID, name, year, gdp);
+    if(addDelete === 'Add'){
+      updateData(userID, name, year, gdp);
+    } else {
+      deleteData(userID, name, year);
+    }
+
   }
-  // chart3 = await createChart()
+
+  customCountries = await getCustomCountries(userID);
+  console.log(customCountries);
+  for(const customCountry of customCountries) {
+    const customOption = new Option(customCountry, customCountry);
+    customSelect.add(customOption);
+    
+  }
+
+  chart3 = await createChartSingle("", 'lineChart3', userID);
 });
 
 countrySelect.addEventListener("change", async (event) => {
@@ -271,6 +436,12 @@ countrySelect.addEventListener("change", async (event) => {
   chart2.destroy();
   chart2 = await createChart(country, 'lineChart2');
 });
+
+customSelect.addEventListener("change", async (event) => {
+  const country = event.target.value;
+  chart3.destroy();
+  chart3 = await createChartSingle(country, 'lineChart3', currentUser.uid);
+})
 
 
 
